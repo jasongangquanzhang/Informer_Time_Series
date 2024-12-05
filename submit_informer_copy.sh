c16f8f543b4d078f01042acdb28cbf35b26c5ba5
Jason Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2024-12-04 21:06:58 -0600

    new file:   csv_results/result_6.csv 	modified:   run.py 	new file:   run_copy.py 	new file:   submit_informer_copy.sh

---
+++ submit_informer_copy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=pi-dachxiu
+#SBATCH --time=0-12:00:00
+#SBATCH --job-name=informer_TS
+#SBATCH --partition=amd
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-100
+#SBATCH --mem-per-cpu=8000
+
+module load python/anaconda-2023.09
+source activate ganett
+
+echo "Array ID: $SLURM_ARRAY_TASK_ID"
+
+srun python3 /home/gangquanz/Informer_Time_Series/run_copy.py $SLURM_ARRAY_TASK_ID
