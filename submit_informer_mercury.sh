00621335f984b770c217efb8cbec6bb2803e4f12
gangquan_zhang <gangquan.zhang@mail.utoronto.ca>
2024-12-04 23:48:09 -0600

    modified:   submit_informer_mercury.sh

--- submit_informer_mercury.sh
+++ submit_informer_mercury.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
-#SBATCH --account=pi-dachenxiu
+#SBATCH --account=pi-dachxiu
 #SBATCH --mem=10G
 #SBATCH --time=0-12:00:00
-#SBATCH --job-name=enverus
+#SBATCH --job-name=informer_TS
 #SBATCH --partition=standard
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-100
 
-module load python/booth/3.8
-
+module load python/booth/3.12
+source ~/venv/gangquanz/bin/activate
 echo "Array ID: $SLURM_ARRAY_TASK_ID"
 
 srun python3 /home/gangquanz/Informer_Time_Series/run_mercury.py $SLURM_ARRAY_TASK_ID
