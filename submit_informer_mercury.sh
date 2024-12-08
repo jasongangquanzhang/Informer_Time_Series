33dbce1281732eb0e2ddb39c1c8e1f785e0afbb1
gangquan_zhang <gangquan.zhang@mail.utoronto.ca>
2024-12-08 14:04:56 -0600

    modified:   submit_informer_mercury.sh

--- submit_informer_mercury.sh
+++ submit_informer_mercury.sh
@@ -11,4 +11,4 @@ module load python/booth/3.12
 source ~/venv/gangquanz/bin/activate
 echo "Array ID: $SLURM_ARRAY_TASK_ID"
 
-srun python3 /home/gangquanz/Informer_Time_Series/run_mercury.py $SLURM_ARRAY_TASK_ID
+srun python3 /home/gangquanz/Informer_Time_Series/run.py $SLURM_ARRAY_TASK_ID
