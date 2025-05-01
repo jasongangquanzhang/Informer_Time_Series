4426435e108a356afc757274930f77ee0e33830d
Jason Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2025-05-01 14:07:43 -0500

    Update run_conjecture.py to change plot directory number and modify submit_informer.sh for correct script execution

--- submit_informer.sh
+++ submit_informer.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=amd
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --array=101-200
+#SBATCH --array=1-100
 #SBATCH --mem-per-cpu=8000
 
 module load python/anaconda-2023.09
@@ -14,4 +14,4 @@ source activate ganett
 
 echo "Array ID: $SLURM_ARRAY_TASK_ID"
 
-srun /home/gangquanz/.conda/envs/ganett/bin/python3.12 /home/gangquanz/Informer_Time_Series/run_table_1.py $SLURM_ARRAY_TASK_ID
+srun /home/gangquanz/.conda/envs/ganett/bin/python3.12 /home/gangquanz/Informer_Time_Series/run_conjecture.py $SLURM_ARRAY_TASK_ID
