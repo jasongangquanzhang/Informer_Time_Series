2402adc1cc71f4c98b9014b0db73255c3067198d
Jason Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2025-06-02 14:02:43 -0500

    modified:   run_TS_fineTune.py

--- run_TS_fineTune.py
+++ run_TS_fineTune.py
@@ -472,6 +472,48 @@ def fine_tune_predict(data):
     return forecast_result
 
 
+class EarlyStopping:
+    def __init__(self, patience=5, verbose=False, delta=0, path="checkpoint.pth"):
+        """
+        Early stopping to stop the training when the validation loss doesn't improve after a given patience.
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f"EarlyStopping counter: {self.counter} out of {self.patience}")
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        """
+        Save model when validation loss decreases.
+        """
+        if self.verbose:
+            print(
+                f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}). Saving model..."
+            )
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
 
 # Define custom dataset class
 class TimeSeriesDataset(Dataset):
