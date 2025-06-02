b3b624a83ff80d6aeb75247627830c42d551ba53
Jason Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2025-06-02 18:33:59 -0500

    modified:   submit_Moirai.sh

--- submit_Moirai.sh
+++ submit_Moirai.sh
@@ -11,7 +11,7 @@
 
 module load python/anaconda-2023.09
 source activate ganett
-
+export HF_ENDPOINT=https://hf-mirror.com
 echo "Array ID: $SLURM_ARRAY_TASK_ID"
 
 srun /home/gangquanz/.conda/envs/ganett/bin/python3.12 /home/gangquanz/Informer_Time_Series/run_TS_fineTune.py $SLURM_ARRAY_TASK_ID
