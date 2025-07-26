c4a928ac6e6c61aa6b1acf445127603e1b288d46
Jason Gangquan Zhang <gangquan.zhang@mail.utoronto.ca>
2025-07-26 15:26:48 -0500

    modified:   TimesFM/log.txt 	modified:   TimesFM/run_TimesFM.py
    Changes not staged for commit:
    	modified:   uni2ts (untracked content)

--- TimesFM/run_TimesFM.py
+++ TimesFM/run_TimesFM.py
@@ -892,7 +892,7 @@ if __name__ == "__main__":
     informer_len = [(10, 2), (20, 4), (50, 10)]
     lr_lst = [1e-4, 1e-3, 1e-2]  
     
-    num = 1
+    num = 6
     plot_dir = f"pretrained_val_plots_{num}"
     os.makedirs(plot_dir, exist_ok=True)
 
